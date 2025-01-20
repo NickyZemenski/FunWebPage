263d6e246bbc4caf6bd2747b5b8212e674c18c91
nicky zemenski <zemen12345@gmail.com>
2025-01-20 04:46:57 -0500

    after section 1

---
+++ UdemyWebPage/Models/CategoryModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemyWebPage.Models
+{
+    public class CategoryModel
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
