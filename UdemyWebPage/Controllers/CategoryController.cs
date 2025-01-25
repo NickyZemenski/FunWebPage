5261c50912d05917208d8730cccd72b0b2281dc7
nicky zemenski <zemen12345@gmail.com>
2025-01-25 08:18:36 -0500

    Category Crud working

--- UdemyWebPage/Controllers/CategoryController.cs
+++ UdemyWebPage/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using UdemyWebPage.Data;
 using UdemyWebPage.Models;
 
@@ -15,6 +16,96 @@ namespace UdemyWebPage.Controllers
         {
             List<CategoryModel> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
+        }    
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]     
+        
+        public IActionResult Create(CategoryModel obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())      // custom validation
+            {
+                ModelState.AddModelError("name", "Name and Display Number Cant match");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+           
+                return View();
+            
+        }
+        public IActionResult Edit(int? categoryId)
+        {
+            if (categoryId == null || categoryId == 0) { 
+                return NotFound();
+            }
+            //  CategoryModel? categoryFromDb = _db.Categories.Find(categoryId);    Both this and next line do the same thing just different ways to do so.
+            //   CategoryModel? categoryFromDb = _db.Categories.Where(u=>u.CategoryId==categoryId).FirstOrDefault(); 
+            CategoryModel? categoryFromDb = _db.Categories.FirstOrDefault(u=>u.CategoryId== categoryId); 
+            if (categoryFromDb == null)
+            {
+                return NotFound(); 
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit(CategoryModel obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())      // custom validation
+            {
+                ModelState.AddModelError("name", "Name and Display Number Cant match");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+        public IActionResult Delete(int? categoryId)
+        {
+            if (categoryId == null || categoryId == 0)
+            {
+                return NotFound();
+            }
+            
+
+            CategoryModel? categoryFromDb = _db.Categories.FirstOrDefault(u => u.CategoryId == categoryId);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+
+        public IActionResult DeletePOST(int? categoryId)
+        {
+            CategoryModel? obj = _db.Categories.Find(categoryId);
+            if (obj.Name == obj.DisplayOrder.ToString())      // custom validation
+            {
+                ModelState.AddModelError("name", "Name and Display Number Cant match");
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+          
+
         }
     }
 }
--- UdemyWebPage/Models/CategoryModel.cs
+++ UdemyWebPage/Models/CategoryModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace UdemyWebPage.Models
 {
@@ -7,7 +8,11 @@ namespace UdemyWebPage.Models
         [Key]
         public int CategoryId { get; set; }
         [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,300,ErrorMessage = "Display order must be between 1-300")]
         public int DisplayOrder { get; set; }
     }
 }
