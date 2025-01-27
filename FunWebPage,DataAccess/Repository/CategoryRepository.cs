ea025eacf24949de883c17bc6be603ff0225ba35
nicky zemenski <zemen12345@gmail.com>
2025-01-27 04:58:37 -0500

    Added IRepository

---
+++ FunWebPage,DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using FunWebPage.DataAccess.Data;
+using FunWebPage.Models;
+using FunWebPage_DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWebPage_DataAccess.Repository
+{
+    public class CategoryRepository : Repository<CategoryModel>,ICategoryRepository
+    {
+        private ApplicationDbContect _db;
+       
+        public CategoryRepository(ApplicationDbContect db):base(db) 
+        {
+            _db = db;
+            
+        }
+        public void Save()
+        {
+           _db.SaveChanges();
+        }
+
+        public void Update(CategoryModel category)
+        {
+            _db.Categories.Update(category);
+        }
+    }
+}
---
+++ FunWebPage,DataAccess/Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using FunWebPage.DataAccess.Data;
+using FunWebPage_DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+namespace FunWebPage_DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContect _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContect db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+            
+        }
+
+        public void Add(T entity)
+        {
+           
+           dbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query= query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList(); 
+        }
+    }
+}
