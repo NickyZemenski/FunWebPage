68122c872f0accf976a14f803531afe93ecf201a
nicky zemenski <zemen12345@gmail.com>
2025-01-30 06:16:11 -0500

    Added Details page and fixed main page

--- UdemyWebPage/Areas/Customer/Controllers/HomeController.cs
+++ UdemyWebPage/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FunWebPage.Models;
+using FunWebPage_DataAccess.Repository.IRepository;
 
 namespace FunWebPage.Areas.Customer.Controllers
 {
@@ -9,14 +10,23 @@ namespace FunWebPage.Areas.Customer.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<ProductModel> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+        public IActionResult Details(int productId)
+        {
+            ProductModel product = _unitOfWork.Product.Get(u=>u.ProductId==productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
