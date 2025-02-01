7d4e658279219f103c8b739caeb57a76db1349d0
nicky zemenski <zemen12345@gmail.com>
2025-02-01 17:06:38 -0500

    Assign Roles Added(to be moved to separate page later)

---
+++ FunWebPage.Utility/EmailSender.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWebPage.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Task.CompletedTask; //send email
+           // throw new NotImplementedException();
+        }
+    }
+}
--- UdemyWebPage/Program.cs
+++ UdemyWebPage/Program.cs
@@ -3,6 +3,8 @@ using FunWebPage.DataAccess.Data;
 using FunWebPage_DataAccess.Repository.IRepository;
 using FunWebPage_DataAccess.Repository;
 using Microsoft.AspNetCore.Identity;
+using FunWebPage.Utility;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,9 +13,10 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContect>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContect>();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContect>().AddDefaultTokenProviders();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
+builder.Services.AddScoped<IEmailSender,EmailSender>();
 
 var app = builder.Build();
 
